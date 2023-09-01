b113df45e0ed501bb38592f3e5a5a9855b489679
YaredP <yaredperezgomez@rocketmail.com>
2023-08-31 22:08:29 -0600

    quinta versión

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-/*const operations = document.querySelector(".operations");*/
 const operations = document.getElementById("operations");
-operations.style.display = "block";
-/*const caja1 = document.querySelector(".caja1");*/
+/*caja1.style.display = "block";*/
 /*const caja1 = document.getElementById("caja1");
 caja1.style.display = "none";*/
+/*const operations = document.getElementById("operations");
+operations.style.display = "block";*/
+/*const caja1 = document.querySelector(".caja1");*/
 const consultar = document.getElementById("consultar");
 const depositar = document.getElementById("depositar");
-const retirar = document.getElementById("retirar");
+const retira = document.getElementById("retirar");
 const cerrar = document.getElementById("cerrar-sesión");
-const saldo = document.getElementById("saldo")
+const saldo = document.getElementById("saldo");
+const cantidad = document.getElementById("cantidad");
+let usuario = null;
 
 var cuentas = [
   {
@@ -37,59 +40,110 @@ const password = document.getElementById("input-password");
 const advertencia = document.getElementById("advertencia");
 const button = document.getElementById("iniciar");
 const saludo = document.getElementById("saludo");
-const validacion = function (event) 
-{
-  event.preventDefault();
+const validacion = function (event){
+event.preventDefault();
+/*document.caja1.style.display = "none";
+document.operations.style.display = "block"; */
   if (
     username.value === cuentas[0].nombre &&
     password.value === cuentas[0].password
-    ) {
-      advertencia.style.color = "green";
-      advertencia.textContent = "Correcto";
-      /*caja1.classList.add("ocultar");*/
-      /*operations.classList.remove("ocultar");*/
-      caja1.style.display = "none";
-      operations.style.display = "block";
+  ) {
+    usuario = cuentas[0];
+    advertencia.style.color = "green";
+    advertencia.textContent = "Correcto";
+    caja1.style.display = "none";
+    operations.style.display = "block";
     saludo.textContent = `Hola ${username.value}`;
     saldo.textContent = cuentas[0].saldo;
   } else if (
     username.value === cuentas[1].nombre &&
     password.value === cuentas[1].password
-    ) {
-      advertencia.style.color = "green";
-      advertencia.textContent = "Correcto";
-      /*caja1.style.display = "none";*/
-      caja1.style.display = "none";
-      operations.style.display = "block";
-    saludo.texContent = `Hola ${username.value}`;
+  ) {
+    usuario = cuentas[1];
+    advertencia.style.color = "green";
+    advertencia.textContent = "Correcto";
+    caja1.style.display = "none";
+    operations.style.display = "block";
+    saludo.textContent = `Hola ${username.value}`;
     saldo.textContent = cuentas[1].saldo;
-
   } else if (
     username.value === cuentas[2].nombre &&
     password.value === cuentas[2].password
-    ) {
-      advertencia.style.color = "green";
-      advertencia.textContent = "Correcto";
-      /*caja1.style.display = "none";*/
-      caja1.style.display = "none";
+  ) {
+    usuario = cuentas[2];
+    advertencia.style.color = "green";
+    advertencia.textContent = "Correcto";
+    caja1.style.display = "none";
     operations.style.display = "block";
-    saludo.texContent = `Hola ${username.value}`;
+    saludo.textContent = `Hola ${username.value}`;
     saldo.textContent = cuentas[2].saldo;
   } else {
     advertencia.style.color = "red";
     advertencia.textContent = "Intenta de nuevo";
-  }};
+  }
+};
+
+// vamos a probar ...
+
+/*iniciar.addEventListener("click",(e) => {
+  console.log("Si escuche tu click!")
+  document.operations.style.display="block";
+  document.caja1.style.display="none";
+})*/
+
+function consultarSaldo(event) {
+  event.preventDefault();
+
+  saldo.textContent = usuario.saldo;
+}
+
+function depositarSaldo(event) {
+  event.preventDefault();
+  if (usuario.saldo >= 990) {
+    advertencia2.style.color = "red";
+    advertencia2.textContent = "monto máximo";
+  } else {
+    const tmp = usuario.saldo + parseInt(cantidad.value);
+    if (tmp >= 990) {
+      advertencia2.style.color = "red";
+      advertencia2.textContent = "monto máximo";
+    } else {
+      usuario.saldo += parseInt(cantidad.value);
+    }
+  }
+}
+
+function retirarSaldo(event) {
+  event.preventDefault();
+  if (usuario.saldo <= 10) {
+    advertencia2.style.color = "red";
+    advertencia2.textContent = "monto mínimo";
+  } else {
+    const tmp = usuario.saldo - parseInt(cantidad.value);
+    if (tmp <= 10) {
+      advertencia2.style.color = "red";
+      advertencia2.textContent = "monto mínimo";
+    } else {
+      usuario.saldo -= parseInt(cantidad.value);
+    }
+  }
+}
+function cerrarSesion(event) {
+  console.log("si entro peeeero no quiero funcionar :(")
+  event.preventDefault();
+  operations.style.display="none";
+  caja1.style.display="block";
+  
+}
 
 button.addEventListener("click", validacion);
-button.addEventListener("click", consultar);
-button.addEventListener("click", depositar);
-button.addEventListener("click", retirar);
-button.addEventListener("click", cerrar-sesión);
+consultar.addEventListener("click", consultarSaldo);
+depositar.addEventListener("click", depositarSaldo);
+retirar.addEventListener("click", retirarSaldo);
+cerrarsesion.addEventListener("click", cerrarSesion);
 
-
-
-operations.style.display = "block";
-
+/*operations.style.display = "block";
+caja1.style.display = "none";*/
 
 console.log(username);
 console.log(password);
